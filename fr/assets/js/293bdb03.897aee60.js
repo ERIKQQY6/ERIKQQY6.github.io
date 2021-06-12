e7528e2f87d05ea8e2f48419f30cb7629fef6d17
ErikQQY <52615090+ErikQQY@users.noreply.github.com>
2021-06-12 16:49:02 +0000

    Deploying to main from @ ERIKQQY6/ERIKQQY6-website@4fdcdf9119870fcfbe10d2a3694e4cbe3e81a87d 🚀

---
+++ fr/assets/js/293bdb03.897aee60.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{78:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/fr/blog/welcome"}]}')}}]);
